2a5b91d16e0fc1b351a6f19a349317775eeb2d91
voidexiled <l20070518@cdmadero.tecnm.mx>
2023-09-23 15:47:17 -0600

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,34 @@
 "use client";
 import React from "react";
 import { motion } from "framer-motion";
-
+import { Nunito_Sans } from "next/font/google";
+const nunito_sans = Nunito_Sans({
+  subsets: ["latin"],
+  weight: ["300", "400", "600"],
+});
 export default function Footer() {
   return (
-    <motion.footer className="text-center opacity-80 font-thin mt-auto py-4 font-nunito-sans text-xl md:text-xl lg:text-base">
-      <motion.small>Copyright &copy; 2023 | All rights reserved</motion.small>
+    <motion.footer
+      className={`${nunito_sans.className} text-center opacity-80 mt-auto py-4 font-nunito-sans text-xl md:text-xl lg:text-xl`}
+    >
+      <div>
+        <span className="text-light">
+          Designed with ♥ & By{" "}
+          <a
+            href="http://github.com/voidexiled"
+            target="_blank"
+            className="opacity-100 text-light-400"
+          >
+            Jesus Jalomo
+          </a>
+        </span>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
     </motion.footer>
   );
 }
