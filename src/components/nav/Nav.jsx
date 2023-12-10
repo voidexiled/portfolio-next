6c0c15c24a3d6645162619ef0759dd2509d4707a
voidexiled <l20070518@cdmadero.tecnm.mx>
2023-12-09 20:04:12 -0600

    Add scroll animation to navbar background

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -46,101 +46,97 @@ export default function Nav() {
   };
 
   return (
-    <motion.header className="">
+    <motion.header className="navbar sticky top-0 z-40 scroll ">
       <motion.nav
-        className={`relative flex flex-col h-[48px] min-h-[48px] max-h-[400px] justify-between items-center text-[18px] non-selectable w-full md:w-[700px] xl:w-[800px] 2xl:w-[1200px] 3xl:w-[1500px] mx-auto ${nunito_sans.className} `}
+        className={`relative min-h-[48px] text-[18px] non-selectable md:w-[700px] xl:w-[800px] 2xl:w-[1200px] 3xl:w-[1500px] mx-auto flex flex-row w-full h-12 justify-between items-center  md:py-9 ${nunito_sans.className} `}
       >
-        <motion.div className="flex flex-row w-full h-12 justify-between items-center  p-4 lg:px-8 md:py-9">
-          <motion.div>
-            <Link href={"/"}>
-              <motion.h1
-                className="hover:text-secondary-400 transition-all"
+        <motion.div>
+          <Link href={"/"}>
+            <motion.h1
+              className="hover:text-secondary-400 transition-all"
+              onClick={() => {
+                setSelected(DIRS.HOME);
+                //handleThemeSwitch();
+              }}
+            >
+              Hi 😁
+            </motion.h1>
+          </Link>
+        </motion.div>
+        <motion.div className="flex">
+          <motion.ul className="flex flex-row space-x-4  md:space-x-8  ">
+            <motion.li
+              className={`relative hover:text-secondary-400 transition-all ${
+                selected == DIRS.HOME ? selectedColorText : notSelectedColorText
+              }
+                `}
+            >
+              <Link
+                href="/"
                 onClick={() => {
                   setSelected(DIRS.HOME);
-                  //handleThemeSwitch();
                 }}
               >
-                Hi 😁
-              </motion.h1>
-            </Link>
-          </motion.div>
-          <motion.div className="flex">
-            <motion.ul className="flex flex-row space-x-4  md:space-x-8  ">
-              <motion.li
-                className={`relative hover:text-secondary-400 transition-all ${
-                  selected == DIRS.HOME
-                    ? selectedColorText
-                    : notSelectedColorText
-                }
-                `}
-              >
-                <Link
-                  href="/"
-                  onClick={() => {
-                    setSelected(DIRS.HOME);
-                  }}
-                >
-                  Home
-                </Link>
-                {selected == DIRS.HOME ? (
-                  <motion.div
-                    layoutId="underline"
-                    transition={{ type: "spring", stiffness: 500, damping: 30 }}
-                    className="absolute top-full left-0 w-full h-[3px] bg-secondary rounded-lg"
-                  />
-                ) : null}
-              </motion.li>
+                Home
+              </Link>
+              {selected == DIRS.HOME ? (
+                <motion.div
+                  layoutId="underline"
+                  transition={{ type: "spring", stiffness: 500, damping: 30 }}
+                  className="absolute top-full left-0 w-full h-[3px] bg-secondary rounded-lg"
+                />
+              ) : null}
+            </motion.li>
 
-              <motion.li
-                className={`relative hover:text-secondary-400 transition-all ${
-                  selected == DIRS.PROJECTS
-                    ? selectedColorText
-                    : notSelectedColorText
-                }
+            <motion.li
+              className={`relative hover:text-secondary-400 transition-all ${
+                selected == DIRS.PROJECTS
+                  ? selectedColorText
+                  : notSelectedColorText
+              }
                 `}
+            >
+              <Link
+                onClick={() => {
+                  setSelected(DIRS.PROJECTS);
+                }}
+                href="/projects"
               >
-                <Link
-                  onClick={() => {
-                    setSelected(DIRS.PROJECTS);
-                  }}
-                  href="/projects"
-                >
-                  Projects
-                </Link>
-                {selected == DIRS.PROJECTS ? (
-                  <motion.div
-                    layoutId="underline"
-                    transition={{ type: "spring", stiffness: 500, damping: 30 }}
-                    className="absolute top-full left-0 w-full h-[3px] bg-secondary rounded-lg"
-                  />
-                ) : null}
-              </motion.li>
-              <motion.li
-                className={`relative hover:text-secondary-400 transition-all ${
-                  selected == DIRS.ABOUT
-                    ? selectedColorText
-                    : notSelectedColorText
-                }
+                Projects
+              </Link>
+              {selected == DIRS.PROJECTS ? (
+                <motion.div
+                  layoutId="underline"
+                  transition={{ type: "spring", stiffness: 500, damping: 30 }}
+                  className="absolute top-full left-0 w-full h-[3px] bg-secondary rounded-lg"
+                />
+              ) : null}
+            </motion.li>
+            <motion.li
+              className={`relative hover:text-secondary-400 transition-all ${
+                selected == DIRS.ABOUT
+                  ? selectedColorText
+                  : notSelectedColorText
+              }
                 `}
+            >
+              <Link
+                onClick={() => {
+                  setSelected(DIRS.ABOUT);
+                }}
+                href={"/about"}
               >
-                <Link
-                  onClick={() => {
-                    setSelected(DIRS.ABOUT);
-                  }}
-                  href={"/about"}
-                >
-                  About
-                </Link>
-                {selected == DIRS.ABOUT ? (
-                  <motion.div
-                    layoutId="underline"
-                    transition={{ type: "spring", stiffness: 500, damping: 30 }}
-                    className="absolute top-full left-0 w-full h-[3px] bg-secondary rounded-lg"
-                  />
-                ) : null}
-              </motion.li>
-            </motion.ul>
-          </motion.div>
+                About
+              </Link>
+              {selected == DIRS.ABOUT ? (
+                <motion.div
+                  layoutId="underline"
+                  transition={{ type: "spring", stiffness: 500, damping: 30 }}
+                  className="absolute top-full left-0 w-full h-[3px] bg-secondary rounded-lg"
+                />
+              ) : null}
+            </motion.li>
+          </motion.ul>
         </motion.div>
       </motion.nav>
     </motion.header>
