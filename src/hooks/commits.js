1e3f6806397fb67ac93e0f86450aa9749f4a29e3
voidexiled <l20070518@cdmadero.tecnm.mx>
2023-12-12 11:35:49 -0600

    Add useSelectedCommit hook for managing selected commit state

---
+++ src/hooks/commits.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+
+const useSelectedCommit = () => {
+  const [selectedCommit, setSelectedCommit] = useState(null);
+
+  useEffect(() => {
+    const storedSelectedCommit = localStorage.getItem("selectedCommit");
+    if (storedSelectedCommit) {
+      setSelectedCommit(JSON.parse(storedSelectedCommit));
+    }
+  }, []);
+
+  const updateSelectedCommit = (newCommit) => {
+    setSelectedCommit(newCommit);
+    localStorage.setItem("selectedCommit", JSON.stringify(newCommit));
+    //console.log(localStorage.getItem("selectedCommit").toString());
+  };
+
+  const clearSelectedCommit = () => {
+    setSelectedCommit(null);
+    localStorage.removeItem("selectedCommit");
+  };
+
+  return { selectedCommit, updateSelectedCommit, clearSelectedCommit };
+};
+
+export default useSelectedCommit;
